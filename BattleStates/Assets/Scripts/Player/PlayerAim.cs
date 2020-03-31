cff1a5f364652d2336cca4e153e7d92bc60233e3
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-31 23:03:48 +0530

    Some small overnight changes.

---
+++ BattleStates/Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ECS;
+public class PlayerAim : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    void Update()
+    {
+        
+        RaycastHit camHit;
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        Vector3 direction = new Vector3();
+        
+        if (Physics.Raycast(camRay, out camHit)) {
+            Transform objectHit = camHit.transform;
+            direction = transform.position - camHit.point;
+        }
+        
+         RaycastHit hit;
+        if (Physics.Raycast(transform.position, -direction, out hit, Mathf.Infinity, layerMask))
+        {
+            direction.y = 0;
+            Debug.DrawRay(transform.position, -direction * 5, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
